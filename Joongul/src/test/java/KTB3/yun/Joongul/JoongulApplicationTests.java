ab30929550a996add3bb666188cc80330f530e5e
Yun <mosy2266@naver.com>
2025-10-10 16:35:45 +0900

    Week4 Initial Commit

---
+++ Joongul/src/test/java/KTB3/yun/Joongul/JoongulApplicationTests.java
@@ -0,0 +1,13 @@
+package KTB3.yun.Joongul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoongulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
