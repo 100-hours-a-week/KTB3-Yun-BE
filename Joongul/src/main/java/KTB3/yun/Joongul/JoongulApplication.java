136685d92f76dfd330e4e91ccbdcd318c1222c82
Yun <mosy2266@naver.com>
2025-10-10 16:44:50 +0900

    Week4 Initial Commit

---
+++ Joongul/src/main/java/KTB3/yun/Joongul/JoongulApplication.java
@@ -0,0 +1,13 @@
+package KTB3.yun.Joongul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoongulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoongulApplication.class, args);
+	}
+
+}
