48f13c08b75fe170701c374ed7e5e02d0d014de1
Yun <mosy2266@naver.com>
2025-10-17 20:08:49 +0900

    Feat: CommentResponseDto 유효성 검사 추가

--- Joongul/src/main/java/KTB3/yun/Joongul/comments/dto/CommentUpdateRequestDto.java
+++ Joongul/src/main/java/KTB3/yun/Joongul/comments/dto/CommentUpdateRequestDto.java
@@ -1,10 +1,12 @@
 package KTB3.yun.Joongul.comments.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class CommentUpdateRequestDto {
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 }
--- Joongul/src/main/java/KTB3/yun/Joongul/comments/dto/CommentWriteRequestDto.java
+++ Joongul/src/main/java/KTB3/yun/Joongul/comments/dto/CommentWriteRequestDto.java
@@ -1,10 +1,12 @@
 package KTB3.yun.Joongul.comments.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class CommentWriteRequestDto {
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 }
